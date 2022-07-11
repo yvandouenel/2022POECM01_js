e1c1690758eec5d61fe115b0a3c2985eda54df87
Yvan Douënel <y.douenel@coopernet.fr>
2022-07-11 10:51:12 +0200

    Premier commit avec 2 tests js.

---
+++ js/01test.js
@@ -0,0 +1,27 @@
+// Déclaration avec le mot clé let et affectation de la valeur 12
+let i = 12;
+console.log(`type de i : `, typeof(i));
+
+i = "Hello";
+console.log(`type de i : `, typeof(i));
+
+i = true;
+console.log(`type de i : `, typeof(i));
+
+
+i = null;
+console.log(`type de i : `, typeof(i));
+
+i = undefined;
+console.log(`type de i : `, typeof(i));
+
+i = 0;
+console.log(`type de i : `, typeof(i));
+i = !!i;
+console.log(`type de i : `, typeof(i));
+console.log(` i : `, i);
+
+
+
+
+

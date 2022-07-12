14125da7d336ee8f10cdc37b242ca1f252626001
Yvan Douënel <y.douenel@coopernet.fr>
2022-07-12 12:19:41 +0200

    test de map.

--- js/11testArray.js
+++ js/11testArray.js
@@ -1,13 +1,20 @@
 const fruits = [
   "banane",
   "orange",
-  "Kiwi"
+  "kiwi"
 ];
 
 console.log(`fruits`, fruits);
 console.log(fruits[2]);
 console.log(`Taille du tableau `, fruits.length);
+const fruits_length = fruits.push('cerise');
+console.log(`fruits_length : `, fruits_length);
+console.log(`Element supprimé : `, fruits.pop());
 
 fruits.forEach((fruit, index) => {
   console.log(fruit, index);
 });
+
+const fruits_li = fruits.map(fruit => `<li>${fruit}</li>`);
+console.log(`fruits `, fruits);
+console.log(`fruits_li `, fruits_li);

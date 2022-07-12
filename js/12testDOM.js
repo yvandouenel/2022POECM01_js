d76a4c0dcebb0b92b3a6bcb17fc939f5f1cee449
Yvan Douënel <y.douenel@coopernet.fr>
2022-07-12 17:31:49 +0200

    Correction de l'exercice des boutons dans la nav.

--- js/12testDOM.js
+++ js/12testDOM.js
@@ -6,45 +6,33 @@
  * @param {Array} attributes
  * @return {DomElement}
  */
-function createDOMElement(markupname, text, parent, attributes) {
+function createDOMElement(markupname, text, parent, attributes = []) {
   const dom_element = document.createElement(markupname);
   dom_element.textContent = text;
   parent.appendChild(dom_element);
 
-  attributes.forEach((attribute => {
-    if (attribute.name && attribute.value) {
-      dom_element.setAttribute(attribute.name, attribute.value);
-    }
-  }))
+  if (attributes.length) {
+    attributes.forEach((attribute => {
+      if (attribute.name && attribute.value) {
+        dom_element.setAttribute(attribute.name, attribute.value);
+      }
+    }))
+  }
   return dom_element;
 }
 
-createDOMElement(
-  "p",
-  "texte",
-  document.body,
-  [
-    { name: "class", value: "toto" },
-    { name: "id", value: "titi" },
-    { name: "lang", value: "fr" }
-  ]);
 
-// Récupération d'un élément du dom
 
-let h1 = document.getElementById("h1");
-console.log(`h1 : `, h1);
-h1.remove();
+// Exo boutons dans nav
 
-const h2 = document.createElement("h2");
-document.body.appendChild(h2);
-h2.textContent = "Titre de niveau 2";
-h2.setAttribute("class", "toto");
-h2.setAttribute("id", "titi");
+// Création d'une balise header
+const header = createDOMElement("header", "", document.body);
 
-const li = document.querySelector("ul > li");
-//li.setAttribute("style", "color:red");
+// Création d'une balise nav
+const nav = createDOMElement("nav", "", header);
 
-const lis = document.querySelectorAll("ul > li");
-lis.forEach((li, index) => {
-  if (index % 2) li.setAttribute("style", "color:red");
-})
+// Boucle pour créer les 4 boutons
+for (let i = 0; i < 4; i++) {
+  if (i == 0) createDOMElement("button", `Item ${i}`, nav, [{ name: "style", value: "color:red" }]);
+  else createDOMElement("button", `Item ${i}`, nav);
+}

b87b2ebe0ab15cfff8588e32a2dd6ec779fefd88
Yvan Douënel <y.douenel@coopernet.fr>
2022-07-12 12:29:49 +0200

    Exo filter.

--- js/11testArray.js
+++ js/11testArray.js
@@ -9,7 +9,7 @@ console.log(fruits[2]);
 console.log(`Taille du tableau `, fruits.length);
 const fruits_length = fruits.push('cerise');
 console.log(`fruits_length : `, fruits_length);
-console.log(`Element supprimé : `, fruits.pop());
+//console.log(`Element supprimé : `, fruits.pop());
 
 fruits.forEach((fruit, index) => {
   console.log(fruit, index);
@@ -18,3 +18,5 @@ fruits.forEach((fruit, index) => {
 const fruits_li = fruits.map(fruit => `<li>${fruit}</li>`);
 console.log(`fruits `, fruits);
 console.log(`fruits_li `, fruits_li);
+
+// utiliser filter pour ne garder que les fruits dont l'index est paire
